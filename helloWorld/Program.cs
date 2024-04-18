8b1ff4a3ba890ce14009d9271754cf7f1fe250c2
m.acuna <m.acuna@AR-IT22459>
2024-04-18 16:10:49 -0300

    Add project files.

---
+++ helloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace helloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+                Estamos en programación II de UTN Fra. 
+
+                string saludo = "¡Hola Mundo!"
+    
+                Aprendemos a realizar comentarios en C#.
+            */
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

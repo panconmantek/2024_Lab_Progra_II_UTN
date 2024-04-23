437fa461328c57013ad9e9da30a4df185f5f7f2c
panconmantek <micahl1981@gmail.com>
2024-04-23 18:31:16 -0300

    Sumador

---
+++ sobrecarga_ejercicioI01-class_library/Class1.cs
@@ -0,0 +1,58 @@
+﻿namespace sobrecarga_ejercicioI01_class_library
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public Sumador() : this(0)
+        {
+
+        }
+
+        public long Sumar(long n1, long n2)
+        {
+            this.cantidadSumas++;
+            return n1 + n2;
+        }
+
+        public string Sumar(string s1, string s2)
+        {
+            this.cantidadSumas++;
+            return s1 + s2;
+        }
+
+        public static explicit operator int(Sumador sumador)
+        {
+            return sumador.cantidadSumas;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            /*
+             * Sobrecargar el operador + (suma) con dos operadores de tipo Sumador. 
+             * El resultado será un long correspondiente al resultado de la suma del 
+             * atributo cantidadSumas de cada argumento.           
+             */
+            return (int)s1 + (int)s2;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            /*
+             * Sobrecargar el operador | (pipe) con dos operadores de tipo Sumador. 
+             * Deberá retornar true si ambos sumadores tienen igual valor en el 
+             * atributo cantidadSumas.           
+             */
+            if ((int)s1 == (int)s2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
